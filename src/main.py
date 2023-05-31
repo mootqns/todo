3e9c41fdf5b0f935db00356c3907947f530e8b05
mootqns <mqn360@gmail.com>
2023-05-31 14:29:32 -0700

    makefile, ignore, docs

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+def toggle_fullscreen(event):
+    # Toggle fullscreen mode
+    root.attributes("-fullscreen", not root.attributes("-fullscreen"))
+
+root = tk.Tk()
+
+# Bind Escape key to exit fullscreen mode
+root.bind("<Escape>", toggle_fullscreen)
+
+# Create a Text widget
+text_box = tk.Text(root)
+text_box.pack(fill="both", expand=True)
+
+# Configure the root window for fullscreen
+root.attributes("-fullscreen", True)
+
+root.mainloop()
